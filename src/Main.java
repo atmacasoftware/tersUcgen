94868c8b8a19469c32ab2ae0ea45e5b487524c82
Ahmet Atmaca <atmacaahmet5261@hotmail.com>
2024-02-23 01:33:13 +0300

    ters üçgen programı v.1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Değişkenlerin tanımlanması
+        int basamakSayisi;
+
+        //Scanner sınıfının import edilmesi
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan değer alınması
+        System.out.print("Basamak sayısı giriniz : ");
+        basamakSayisi = input.nextInt();
+
+        for(int i = basamakSayisi; i>=1 ;i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
